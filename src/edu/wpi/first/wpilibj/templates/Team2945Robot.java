0ca115cefd385e4865d50bb5880e799e65f66eea
Bryce Guinta <azrathud@gmail.com>
2013-01-03 14:48:11 -0700

    init

---
+++ src/edu/wpi/first/wpilibj/templates/Team2945Robot.java
@@ -0,0 +1,54 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package edu.wpi.first.wpilibj.templates;
+
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the SimpleRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+
+public class Team2945Robot extends SimpleRobot {
+    /**
+     * This function is called once each time the robot enters autonomous mode.
+     */
+    RobotDrive drive = new RobotDrive(1, 2);
+
+    // madcatz drive is always in port 1
+    Joystick madcatz = new Joystick(1);
+
+    
+    public void autonomous() {
+        getWatchdog().setEnabled(false);
+        drive.drive(1, 0);
+        Timer.delay(2);
+        drive.drive(0, 0);
+
+
+    }
+
+    /**
+     * This function is called once each time the robot enters operator control.
+     */
+    public void operatorControl() {
+        while(true) {
+            getWatchdog().feed();
+
+            drive.tankDrive(madcatz.getRawAxis(2), madcatz.getRawAxis(5));
+        }
+
+    }
+}
